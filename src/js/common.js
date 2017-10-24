5cb71d4943ce411201136c1daf3caedb4b9533ab
Tim <andrei.kurlyandchik@gmail.com>
2017-10-24 18:35:47 +0300

    add home page

--- src/js/common.js
+++ src/js/common.js
@@ -20,7 +20,58 @@ function initDopMenu() {
     })
 }
 
+function initHeaderMenu() {
+    $('.block-menu').on('click', function () {
+        if($('.open-menu').length){
+            $(this).removeClass('open-menu');
+            $(this).addClass('close-menu');
+            $('.block-menu-header').css({
+                "top": "90px"
+            });
+        } else {
+            $(this).removeClass('close-menu');
+            $(this).addClass('open-menu');
+            $('.block-menu-header').css({
+                "top": "-200%"
+            });
+        }
+    });
+}
+
+function initSwiperHeader() {
+    var swiper = new Swiper('.swiper-container-header', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        spaceBetween: 5,
+        speed: 900,
+        autoHeight: true
+    });
+}
+
+function initSwiperShares() {
+    var swiper = new Swiper('.js-shares-sw', {
+        // slidesPerView: 2,
+        slidesPerView: 'auto',
+        spaceBetween: 25,
+        autoHeight: true
+    });
+}
+
+function initSwiperRecipe() {
+    var swiper = new Swiper('.js-recipe-sw', {
+        slidesPerView: 'auto',
+        spaceBetween: 25,
+        autoHeight: true
+    });
+}
+
+initSwiperHeader();
 
 document.addEventListener('DOMContentLoaded', function () {
     initDopMenu();
+    initHeaderMenu();
+    initSwiperShares();
+    initSwiperRecipe();
 });
--- src/js/lib.js
+++ src/js/lib.js
@@ -1,3 +1,3 @@
 //=include lib/jquery.js
 
-//=include lib/barba.js
+//=include lib/swiper.jquery.min.js
