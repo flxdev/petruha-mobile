4e6dc94477552e5ccac746989783dbc35dfdf66d
Tim <andrei.kurlyandchik@gmail.com>
2017-10-30 19:03:04 +0300

    add lk page, stock-page, stock-recipes page

--- bitrix/templates/main/js/common.js
+++ bitrix/templates/main/js/common.js
@@ -118,7 +118,7 @@ function clearDropzone() {
 }
 
 function initTabsCompany() {
-    // var swiperRecipes = $('.tabs-link').data('data-tab-c', 'tab-contact-2');
+    var swiperRecipes = $('.tabs-link').data('data-tab-c', 'tab-contact-2');
 
     $('.row-tabs-company .tabs-link').click(function () {
         var tab_id = $(this).attr('data-tab-c');
@@ -129,9 +129,9 @@ function initTabsCompany() {
         $("#"+tab_id).addClass('current-t');
     });
 
-    // swiperRecipes.click(function () {
-    //     initSwiperRecipes();
-    // });
+    swiperRecipes.click(function () {
+        initSwiperRecipes();
+    });
 }
 
 function initSwiperCompanyB() {
@@ -156,6 +156,136 @@ function initAnchor() {
     });
 }
 
+function initPopup() {
+    var centralBlock = $('.container-white-block');
+    var body = $('body');
+    var feedbackPopup = $('.block-white-feedback');
+    var lkPopup = $('.block-white-tabs');
+    // var feedbackData = $('.js-btn').data('data-popup', 'feedback');
+    // var lkData = $('.js-btn').data('data-popup', 'lk');
+    var closeBtn = $('.js-close');
+
+    $('[data-popup="feedback"]').on('click', function () {
+        centralBlock.css({
+            "left": "0"
+        });
+        body.addClass('body-hidden');
+        feedbackPopup.css({
+            "display": "block"
+        });
+        lkPopup.css({
+            "display": "none"
+        });
+    });
+
+    $('[data-popup="lk"]').on('click', function () {
+        centralBlock.css({
+            "left": "0"
+        });
+        body.addClass('body-hidden');
+        lkPopup.css({
+            "display": "block"
+        });
+        feedbackPopup.css({
+            "display": "none"
+        });
+    });
+
+    closeBtn.on('click', function () {
+        centralBlock.css({
+            "left": "-150%"
+        });
+        body.removeClass('body-hidden');
+        setTimeout(function () {
+            feedbackPopup.css({
+                "display": "none"
+            });
+            lkPopup.css({
+                "display": "none"
+            });
+        }, 500)
+    });
+}
+
+function initTabsForm() {
+    $('.block-white-block_btn-tabs .tab-link').click(function () {
+        var tab_id = $(this).attr('data-tabs');
+
+        $('.block-white-block_btn-tabs .tab-link').removeClass('current-f');
+        $('.wrapper-white-block-tabs').removeClass('current-f');
+
+        $(this).addClass('current-f');
+        $("#" + tab_id).addClass('current-f');
+    })
+}
+
+function initNewPass(){
+    $('.link-pass-no').on('click', function () {
+        $(this).closest('.wrapper-white-block-tabs_form').find('.js-form').removeClass('edit-pass');
+        $(this).closest('.js-form').addClass('edit-pass');
+    });
+}
+
+function initSwiperRecipes() {
+    var swiper = new Swiper('.box-recipes-slider', {
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        pagination: '.swiper-pagination',
+        paginationType: 'progress',
+        simulateTouch: false
+    });
+
+    if($('.number-sl').length){
+        $('.number-sl').text(swiper.slides.length);
+    } else {
+        return
+    }
+
+    $('.number-sl').text(swiper.slides.length);
+
+    $('.js-number-slider').on( "click", function() {
+        $('.number-active').text(swiper.activeIndex + 1);
+    });
+}
+
+function initSwiperIngredients() {
+    var mySwiper = new Swiper('.swiper-container-ingredients', {
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        simulateTouch: false
+    });
+}
+
+function initLk() {
+    $('.btn-change-pass').on('click', function () {
+        $(this).closest('.box-input').find('.input-box').css({
+            "pointer-events": "auto"
+        }).focus();
+    })
+}
+
+function initNewPassLk() {
+    $('.btn-change-pass').on('click', function () {
+        $('.new-pass').removeClass('display-none');
+        $('.js-save').removeClass('disabled-btn');
+        $('.new-pass').find('.input-box').css({
+            "pointer-events": "auto"
+        })
+    });
+
+    $('.js-form').each(function () {
+        // var disBtn = $(this).hasClass('disabled-btn');
+        var disBtn = $('.disabled-btn').length;
+        if (disBtn === 1){
+            $('.input-box').focus(function () {
+                $('.btn-form').removeClass('disabled-btn');
+            })
+        }
+    })
+}
+
 
 initSwiperHeader();
 initDropzoneCompany();
@@ -169,4 +299,11 @@ document.addEventListener('DOMContentLoaded', function () {
     initTabsCompany();
     initSwiperCompanyB();
     initAnchor();
+    initPopup();
+    initTabsForm();
+    initNewPass();
+    initSwiperRecipes();
+    initSwiperIngredients();
+    initLk();
+    initNewPassLk();
 });
